3afb123850bb3988161f80eae6c3835cd08d21ab
Kavitoj Singh <kavitojsingh@gmail.com>
2016-11-02 16:02:07 +0000

    Badges And Schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  badge_messages = []
+  for name in array
+    badge_messages << "Hello, my name is #{name}."
+  end
+  badge_messages
+end
+
+
+def assign_rooms(array)
+  room_assignments = []
+  array.each_with_index { |item, index|
+    room_assignments << "Hello, #{item}! You'll be assigned to room #{index + 1}!"
+  }
+  room_assignments
+end
+
+
+def printer(array)
+  badge_messages = batch_badge_creator(array)
+  room_assignments = assign_rooms(array)
+  badge_messages.each do |line|
+  	puts line.chomp
+  end
+  room_assignments.each do |line|
+  	puts line.chomp
+  end
+  nil
+end
